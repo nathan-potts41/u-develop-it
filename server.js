f60c6547270dfa3d902bbdae4cd4e060fafef512
nathan-potts41 <pottsandpans41@hotmail.com>
2021-01-06 14:24:29 -0600

    added create a candidate post

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const sqlite3 = require('sqlite3').verbose();
+const inputCheck = require('./utils/inputCheck');
 
 //Middleware
 app.use(express.urlencoded({ extended: false }));
@@ -75,7 +76,28 @@ app.get('/api/candidate/:id', (req, res) => {
     });
 });
 
-
+//Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+                VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    db.run(sql, params, function (err, result) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body,
+            id: this.lastID
+        });
+    });
+});
 
 
 //default response for any other request(Not Found) Catch All
